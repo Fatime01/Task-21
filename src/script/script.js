f3a48f7ed30a82d3c599d2d0211e78807f43c73f
Fatime01 <memiyevafatime280@gmail.com>
2024-05-16 11:44:11 +0400

    1 filed changed

--- src/script/script.js
+++ src/script/script.js
@@ -1,50 +1,53 @@
-// function findAvarage(arr) {
-//     let sum = 0;
-//     for (let i = 0; i < arr.length; i++) {
-//         sum += arr[i];
-//     }
-//     return sum / arr.length;
-// }
-// const arr = [1, 5, 10, 15, 20];
-// console.log(findAvarage(arr));
+//(Task1)
+function findAvarage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+const arr = [1, 5, 10, 15, 20];
+console.log(findAvarage(arr));
+
+//(Task2)
+function anyFunction(str) {
+    let resulte = '';
+
+    for (let i = str.length -1; i >= 0; i--) {
+        resulte += str[i]
+    }
+
+    return resulte
+}
+console.log(anyFunction('Connect'));
 
 
-// function anyFunction(str) {
-//     let resulte = '';
+//Task 3 u 2 yolla yazmisam
+//1(Task3)
+function firstFunction(str) {
+    let num = Number(prompt('?'))
+    let cavab = 1
+    for(let i = 2; i < num; i++){
+    cavab *= i
+    }
+    return cavab
+}
+console.log(firstFunction());
 
-//     for (let i = str.length -1; i >= 0; i--) {
-//         resulte += str[i]
-//     }
+//2(Task3)
+const faktorial = (num) => {
+    let answer = 1
 
-//     return resulte
-// }
-// console.log(anyFunction('Connect'));
+    for (let i = 2; i < num; i++){
+        answer *= i
+    }
 
+    console.log(answer);
+}
 
-// function firstFunction(str) {
-//     let num = Number(prompt())
-//     let cavab = 1
-//     for(let i = 2; i <=num; i++){
-//         resulte += str(i)
-//     cavab *= i
-//     }
-//     return resulte
-// }
-// console.log(firstFunction(`${num}! = ${cavab}`));
+faktorial(6)
 
-
-// const faktorial = (num) => {
-//     let answer = 1
-
-//     for (let i = 2; i < num; i++){
-//         answer *= i
-//     }
-
-//     console.log(answer);
-// }
-
-// faktorial(6)
-
+//(Task4)
 const second = (arr) => {
  let result = ''
  let abc = arr.length
@@ -62,3 +65,16 @@ const second = (arr) => {
 }
 
 second(['apple', 'orange', "strawberry"])
+
+//(Task5)
+const sum = (num) =>{
+    let str = num.toString()
+    let result = 0
+
+    for(let i = 0; i < str.length; i++){
+        result += Number(str[i])
+    }
+    console.log(result);
+}
+
+sum(5589)
